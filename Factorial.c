71923b51c68ab29575c74eec81f3104c8309922c
JeetPatel7 <jeet.d.kyada@gmail.com>
2025-07-15 22:49:33 +0530

    Add files via upload

---
+++ Factorial.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int num, i;
+    double factorial = 1; 
+
+    
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    
+    if (num < 0) {
+        printf("Error: Factorial of a negative number doesn't exist.\n");
+    } else {
+        for (i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+
+        printf("Factorial of %d is : %.2lf\n", num, factorial);
+    }
+
+    return 0;
+}
